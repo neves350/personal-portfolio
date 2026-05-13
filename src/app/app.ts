3b2ea56be1033bf97d28c6f44ad7fb05e7220445
neves350 <martimneves350@gmail.com>
2026-05-13 18:41:30 +0100

    feat: add navbar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { Background } from './layout/background/background';
+import { Navbar } from "./layout/navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Background],
+  imports: [RouterOutlet, Background, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
