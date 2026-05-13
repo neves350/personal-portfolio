b9805d23c7a4d8806f623372c26ba0b8fd23323f
neves350 <martimneves350@gmail.com>
2026-05-13 16:05:16 +0100

    feat: add sheet component

---
+++ src/app/shared/components/sheet/index.ts
@@ -0,0 +1,9 @@
+export {
+	type OnClickCallback as SheetOnClickCallback,
+	ZardSheetComponent,
+	ZardSheetOptions,
+} from '@/shared/components/sheet/sheet.component'
+export * from '@/shared/components/sheet/sheet.imports'
+export * from '@/shared/components/sheet/sheet.service'
+export * from '@/shared/components/sheet/sheet.variants'
+export * from '@/shared/components/sheet/sheet-ref'
