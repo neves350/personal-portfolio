95cbb9161fcc1240a87c7b602dd9579f59e571fa
neves350 <martimneves350@gmail.com>
2026-05-14 09:49:56 +0100

    feat: create hero section

---
+++ src/app/sections/hero/hero.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core'
+import { HeroActions } from './hero-actions/hero-actions'
+import { HeroBio } from './hero-bio/hero-bio'
+import { HeroFocus } from './hero-focus/hero-focus'
+import { HeroHeader } from './hero-header/hero-header'
+import { HeroProfile } from './hero-profile/hero-profile'
+import { HeroSocial } from './hero-social/hero-social'
+
+@Component({
+	selector: 'app-hero',
+	imports: [
+		HeroProfile,
+		HeroHeader,
+		HeroBio,
+		HeroSocial,
+		HeroActions,
+		HeroFocus,
+	],
+	templateUrl: './hero.html',
+	styleUrl: './hero.css',
+})
+export class Hero {}
