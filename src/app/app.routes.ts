9bafb599cc2662e17e75cc2f723c56e38d785408
neves350 <martimneves350@gmail.com>
2026-05-14 20:16:05 +0100

    feat: add projects route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,19 @@ import { Routes } from '@angular/router'
 export const routes: Routes = [
 	{
 		path: '',
-		title: 'Home',
+		title: 'Martim Neves',
 		loadComponent: () => import('./pages/main/main').then((m) => m.Main),
 	},
 	{
 		path: 'contact',
-		title: 'Contact',
+		title: 'Contact | Martim Neves',
 		loadComponent: () =>
 			import('./pages/contact/contact').then((m) => m.Contact),
+	},
+	{
+		path: 'projects',
+		title: 'Projects | Martim Neves',
+		loadComponent: () =>
+			import('./pages/project/project').then((m) => m.Project),
 	},
 ]
