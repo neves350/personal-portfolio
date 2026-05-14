29ca9828513ec398f9a07a78a806b84574511a8d
neves350 <martimneves350@gmail.com>
2026-05-14 10:17:33 +0100

    feat: add footer component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { Background } from './layout/background/background'
+import { Footer } from './layout/footer/footer'
 import { Navbar } from './layout/navbar/navbar'
 import { Hero } from './sections/hero/hero'
 
 @Component({
 	selector: 'app-root',
-	imports: [RouterOutlet, Background, Navbar, Hero],
+	imports: [RouterOutlet, Background, Navbar, Hero, Footer],
 	templateUrl: './app.html',
 	styleUrl: './app.css',
 	changeDetection: ChangeDetectionStrategy.OnPush,
