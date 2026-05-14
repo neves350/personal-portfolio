8cacbd4423f202da53f11ea6444d9247187836e3
neves350 <martimneves350@gmail.com>
2026-05-14 09:50:09 +0100

    feat: add hero section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-import { Background } from './layout/background/background';
-import { Navbar } from "./layout/navbar/navbar";
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { Background } from './layout/background/background'
+import { Navbar } from './layout/navbar/navbar'
+import { Hero } from './sections/hero/hero'
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Background, Navbar],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'app-root',
+	imports: [RouterOutlet, Background, Navbar, Hero],
+	templateUrl: './app.html',
+	styleUrl: './app.css',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {}
