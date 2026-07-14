67149a218410477ceef827abb13e242fe9e94022
neves350 <martimneves350@gmail.com>
2026-07-14 17:00:19 +0100

    style: update routes titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,18 @@ import { Routes } from '@angular/router'
 export const routes: Routes = [
 	{
 		path: '',
-		title: 'Martim Neves',
+		title: 'Martim Neves - Portfolio',
 		loadComponent: () => import('./pages/main/main').then((m) => m.Main),
 	},
 	{
 		path: 'contact',
-		title: 'Contact | Martim Neves',
+		title: 'Martim Neves - Contact',
 		loadComponent: () =>
 			import('./pages/contact/contact').then((m) => m.Contact),
 	},
 	{
 		path: 'projects',
-		title: 'Projects | Martim Neves',
+		title: 'Martim Neves - Projects',
 		loadComponent: () =>
 			import('./pages/project/project').then((m) => m.Project),
 	},
